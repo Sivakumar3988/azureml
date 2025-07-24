a2ff3b00eda40765fb50f6c833a999c32343abdb
Sivakumar3988 <sivakumarbokam22@gmail.com>
2025-07-24 15:47:36 +0530

    Ingestion added and model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,62 +1,77 @@
 import sys
 from dataclasses import dataclass
-import numpy as np
-import pandas as pd 
+
+import numpy as np 
+import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
 import os
 
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
-    def __init(self):
-        self.data_transformation_config = DataTransformationConfig()
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
         '''
-        This function is responsible for data transformation.
-        It returns a preprocessor object that will be used to transform the data.'''
+        This function si responsible for data trnasformation
+        
+        '''
         try:
-            numerical_features = ['writing_score', 'reading_score']
-            categorical_features = [
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
                 "gender",
                 "race_ethnicity",
                 "parental_level_of_education",
                 "lunch",
-                "test_preparation_course"
-
+                "test_preparation_course",
             ]
 
-            num_pipeline = Pipeline(steps=[
-                ('imputer', SimpleImputer(strategy='median')),
-                ('scaler', StandardScaler())
-            ]
-            )
-            cat_pipeline = Pipeline(steps=[
-                ('imputer', SimpleImputer(strategy='most_frequent')),
-                ('onehotencoder', OneHotEncoder()),
-                ('scaler', StandardScaler())
-            ]
+            num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
 
-            )
-            logging.info(f"Numerical features: {numerical_features}")
-            logging.info(f"Categorical features: {categorical_features}")
-            preprocessor = ColumnTransformer(
-                [
-                    ('num_pipeline', num_pipeline, numerical_features),
-                    ('cat_pipeline', cat_pipeline, categorical_features)
                 ]
             )
 
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
+            )
+
             return preprocessor
+        
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
         
     def initiate_data_transformation(self,train_path,test_path):
 
